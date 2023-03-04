1cd951162d5226b9c279e0dafe28851e979a51d1
Dmytro Doroschuk <dimariouz.dev@gmail.com>
2023-03-04 18:16:17 +0200

    create project

---
+++ mobileTask/mobileTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobileTask
+//
+//  Created by Dmitry Doroshchuk on 04.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
